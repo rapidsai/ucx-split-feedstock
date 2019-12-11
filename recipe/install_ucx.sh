5d64cff47472b734b73fb2d835783553dff6ff46
John Kirkham <jakirkham@gmail.com>
2019-12-10 23:26:35 -0600

    Drop flags adding sysroot paths for compilation
    As we are now configuring the build `--with-sysroot`, these flags should
    not be needed any more. So go ahead and drop them and let the build
    figure these out.

--- recipe/install_ucx.sh
+++ recipe/install_ucx.sh
@@ -2,11 +2,6 @@
 
 set -xeuo pipefail
 
-export CONDA_BUILD_SYSROOT="$(${CC} --print-sysroot)"
-
-export CFLAGS="${CFLAGS} -I${CONDA_BUILD_SYSROOT}/usr/include"
-export LDFLAGS="${LDFLAGS} -L${CONDA_BUILD_SYSROOT}/usr/lib64"
-
 CUDA_CONFIG_ARG=""
 if [ ${cuda_compiler_version} != "None" ]; then
     CUDA_CONFIG_ARG="--with-cuda=${CUDA_HOME}"
