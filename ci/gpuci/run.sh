6da94f290e98b1da5e1834d7880126546c5985c8
Mike Wendt <mike.wendt@me.com>
2020-01-31 11:16:19 -0500

    ENH Add run file for gpuCI job

---
+++ ci/gpuci/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2020, NVIDIA CORPORATION.
+#####################################
+# ucx-py conda run script for gpuCI #
+#####################################
+set -ex
+
+# Set paths
+export HOME=$WORKSPACE
+
+# Install gpuCI tools
+curl -s https://raw.githubusercontent.com/rapidsai/gpuci-mgmt/master/gpuci-tools.sh | bash
+source ~/.bashrc
+
+# Fetch docker container
+gpuci_retry docker pull ${FROM_IMAGE}:${CUDA_VERSION}
+
+# Run conda build script
+gpuci_retry docker run ${FROM_IAMGE} ci/cpu/build.sh
