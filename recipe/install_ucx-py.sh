228916cc633aad0408d4c3b4c3649d52d8f3802c
jakirkham <jakirkham@gmail.com>
2020-06-03 16:58:51 -0700

    Ensure dependencies are not installed (#28)
    While conda-build does configure pip to not install dependencies, that
    does not appear to be respected here. So add in our own pip flags to
    ensure that dependencies are not getting installed and included in the
    package.

--- recipe/install_ucx-py.sh
+++ recipe/install_ucx-py.sh
@@ -3,4 +3,5 @@
 set -xeuo pipefail
 
 cd "${SRC_DIR}/ucx-py"
-$PYTHON -m pip install . -vv
+mkdir -p pip_cache
+$PYTHON -m pip install --no-index --no-deps --ignore-installed --cache-dir ./pip_cache . -vv
