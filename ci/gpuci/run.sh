e983bdacc66914fc81a92117083fbf9aba027fc4
Mike Wendt <mike.wendt@me.com>
2020-07-23 09:41:31 -0400

    FIX Update run.sh to use new FROM_IMAGE def that includes tag

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -44,11 +44,7 @@ env > env.list
 
 # Get build container
 gpuci_logger "Pull docker container for build..."
-BUILD_IMAGE="${FROM_IMAGE}:${CUDA_VER}"
-if [ "$CUDA_VER" == "None" ] ; then
-  # Checkout latest instead
-  BUILD_IMAGE="${FROM_IMAGE}:latest"
-fi
+BUILD_IMAGE="${FROM_IMAGE}"
 
 # Run conda build script
 gpuci_logger "Run docker and kick off build script..."
